b0c4a662cad89f61d50b23154df7e8e4386e3d54
JackyLin1 <jlin9702@gmail.com>
2022-02-04 11:45:05 +0900

    added createTweetElemnt function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,58 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+
+
+const createTweetElement = function (data) {
+
+  let $tweet = $(
+    `<section class='box'>
+    <top>
+      <img class='profile' src=${data.user.avatars}>
+      <div class='tweet'>${data.content.text}<hr></div>
+    </top>
+    
+    <p class='name'>${data.user.name}</name></p>
+    <p class='userID'>${data.user.handle}</p>
+    <footer>
+    <p class= 'timestamp'>posted 1s ago</p>
+    <p class='symbols'> 
+      <span class ='thumbs'><i class="far fa-thumbs-up"></i></span>
+      <span class ='retweet'><i class="fas fa-recycle"> </i> </span>
+      <span class ='comment'><i class="far fa-comments"></i> </span> </p>     
+    </footer>
+
+  </section>`
+  )
+  return $tweet;
+}
+
+createTweetElement(data);
+});
