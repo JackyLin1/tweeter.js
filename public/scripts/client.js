9e4b2949f28fb24aaa584ee5ef906579bb2e3dff
JackyLin1 <jlin9702@gmail.com>
2022-02-06 11:24:59 +0900

    added cross scripiting and a few style changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
  */
 $(document).ready(function () {
 
-
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
 //messages goes through createTweetElement
 //new tweets shows on top of old tweets
 const renderTweet = function (tweets) {
@@ -40,14 +14,22 @@ const renderTweet = function (tweets) {
   }
 }
 
+//escape function to re-encode text
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 //Tweet get form into a tweet card
 const createTweetElement = function (data) {
+let tweetContent = `<div class='tweet'>${escape(data.content.text)}<hr></div>`
 
   let $tweet = $(
     `<section class='box'>
     <top>
       <img class='profile' src=${data.user.avatars}>
-      <div class='tweet'>${data.content.text}<hr></div>
+      ${tweetContent}
     </top>
     
     <p class='name'>${data.user.name}</name></p>
@@ -95,6 +77,11 @@ $('form').submit(eventObj => {
     .then($('#errLong').hide())
     .then($('textarea').val(''));
   }
+})
+
+//compose button function, click to show text
+$('#composeBtn').click(function () {
+$('#tweetarea').slideToggle('slow')
 })
 
 //Get JSON in /tweets
