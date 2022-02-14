43c1dd8444bc7becf9b554d31f1888fd0158624d
JackyLin1 <jlin9702@gmail.com>
2022-02-15 04:05:07 +0900

    Fix bug: delay when tweeting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
         data: text,
       })
       // .then(console.log(text))
-        .then(loadTweets())
+        .then(()=>loadTweets())
         .then($('#errShort').hide())
         .then($('#errLong').hide())
         .then($('textarea').val(''))
